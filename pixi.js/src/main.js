b1d7d5cf7b9297382d1349cbc22c69dd402e932b
PragmaticBeaver <pragmaticbeaver@gmx.de>
2020-11-03 20:00:08 +0100

    [pixi.js] mod: formated code with prettier

---
+++ pixi.js/src/main.js
@@ -0,0 +1,7 @@
+import { Application } from "pixi.js";
+
+//Create a Pixi Application
+let app = new Application({ width: 256, height: 256 });
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
