236e5910ba551e0ccb702e678d595adb31560ec8
PragmaticBeaver <pragmaticbeaver@gmx.de>
2020-11-03 21:18:37 +0100

    [pixi] mod: display img

--- pixi.js/src/main.js
+++ pixi.js/src/main.js
@@ -1,7 +1,37 @@
-import { Application } from "pixi.js";
+let app;
 
-//Create a Pixi Application
-let app = new Application({ width: 256, height: 256 });
+const battleImg = "assets/battle.jpg";
 
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
+function loadImg() {
+  PIXI.loader.add(battleImg).load(() => {
+    const sprite = new PIXI.Sprite(PIXI.loader.resources[battleImg].texture);
+    app.stage.addChild(sprite);
+  });
+}
+
+function resize(
+  width = window.innerWidth / 2,
+  height = window.innerHeight / 2
+) {
+  app.renderer.autoDensity = true;
+  app.renderer.resize(width, height);
+}
+
+function init() {
+  app = new PIXI.Application({
+    width: 256,
+    height: 256,
+    antialias: true,
+    transparent: false,
+    resolution: 1,
+  });
+
+  // Add the canvas that Pixi automatically created for you to the HTML document
+  // must be present before any sprites can be shown
+  document.body.appendChild(app.view);
+
+  loadImg();
+}
+
+init();
+resize();
