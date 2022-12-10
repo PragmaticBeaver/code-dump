70c350fb5761bc33f6cdd21631f5100ea07e582d
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2022-12-10 01:55:01 +0100

    Update chatGPT_normalize_audio.py

--- chatGPT_normalize_audio.py
+++ chatGPT_normalize_audio.py
@@ -22,6 +22,15 @@ for root, dirs, files in os.walk(BASE_DIR):
             normalized_audio.export(os.path.join(root, "normalized_" + file), format="mp3")
 
 
-# This script uses the os and pydub modules to normalize audio files within a directory and its subdirectories. It sets the target level for normalization to 0 dB, and it looks for MP3 and WAV files in the ./audio directory (you can change this to the directory where your audio files are located). For each audio file it finds, the script loads the file using pydub, normalizes it using the target level, and saves the normalized audio to a new file.
+# This script uses the os and pydub modules to normalize audio files within a directory and its subdirectories.
+# It sets the target level for normalization to 0 dB, and it looks for MP3 and WAV files in the ./audio directory
+# (you can change this to the directory where your audio files are located).
+# For each audio file it finds, the script loads the file using pydub, normalizes it using the target level,
+# and saves the normalized audio to a new file.
 
-# To use this script, you will need to install the pydub module by running pip install pydub in a terminal or command prompt. You will also need to replace the ./audio directory with the directory where your audio files are located. After you have made these changes, you can run the script by typing python script.py, where script.py is the name of the file containing the script. This will normalize all audio files within the specified directory and its subdirectories.
+# To use this script, you will need to install the pydub module by running "pip install pydub" in a terminal or command prompt.
+# You will also need to replace the ./audio directory with the directory where your audio files are located.
+# After you have made these changes, you can run the script by typing python script.py,
+# where script.py is the name of the file containing the script.
+
+# This will normalize all audio files within the specified directory and its subdirectories.
