ab8293250919608da5772a69c2f83be870db1c07
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2023-01-01 16:38:36 +0100

    remove comments

--- normalize_audio.py
+++ normalize_audio.py
@@ -1,15 +1,9 @@
 import os
 from pydub import AudioSegment
 
-# Set the target level for normalization (in dB)
 TARGET_LEVEL = -20
-
-# Set the base directory for the audio files
 BASE_DIR = "E:\Downloads\Music\Playlists"
 OUT_DIR = "E:\Downloads\Music\\normlized"
-
-# if not os.path.exists(OUT_DIR):
-#     os.makedirs(OUT_DIR)
 
 # Loop through all files and subdirectories in the base directory
 for root, dirs, files in os.walk(BASE_DIR):
@@ -37,17 +31,3 @@ for root, dirs, files in os.walk(BASE_DIR):
             # Save the normalized audio to a new file
             normalized_file_path = os.path.join(out_subdir, file)
             normalized_audio.export(normalized_file_path, format="mp3")
-
-
-# This script uses the os and pydub modules to normalize audio files within a directory and its subdirectories.
-# It sets the target level for normalization to 0 dB, and it looks for MP3 and WAV files in the ./audio directory
-# (you can change this to the directory where your audio files are located).
-# For each audio file it finds, the script loads the file using pydub, normalizes it using the target level,
-# and saves the normalized audio to a new file.
-
-# To use this script, you will need to install the pydub module by running "pip install pydub" in a terminal or command prompt.
-# You will also need to replace the ./audio directory with the directory where your audio files are located.
-# After you have made these changes, you can run the script by typing python script.py,
-# where script.py is the name of the file containing the script.
-
-# This will normalize all audio files within the specified directory and its subdirectories.
