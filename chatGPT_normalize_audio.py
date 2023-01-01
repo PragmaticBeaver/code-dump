6debc16c71eb9560065f2235130e3712fe2f16a3
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2023-01-01 16:04:52 +0100

    Fix normalization script output directories
    subdirs where not created

--- chatGPT_normalize_audio.py
+++ chatGPT_normalize_audio.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from pydub import AudioSegment
 
 # Set the target level for normalization (in dB)
@@ -7,13 +6,19 @@ TARGET_LEVEL = -20
 
 # Set the base directory for the audio files
 BASE_DIR = "E:\Downloads\Music\Playlists"
-TARGET_DIR = "E:\Downloads\Music\\normlized"
+OUT_DIR = "E:\Downloads\Music\\normlized"
 
-if not os.path.exists(TARGET_DIR):
-    os.makedirs(TARGET_DIR)
+# if not os.path.exists(OUT_DIR):
+#     os.makedirs(OUT_DIR)
 
 # Loop through all files and subdirectories in the base directory
 for root, dirs, files in os.walk(BASE_DIR):
+
+    # create output subdirectories if it does not exist
+    out_subdir = os.path.join(OUT_DIR, os.path.relpath(root, BASE_DIR))
+    if not os.path.exists(out_subdir):
+        os.makedirs(out_subdir)
+
     for file in files:
         # Check if the file is an audio file
         if file.endswith(".mp3") or file.endswith(".wav"):
@@ -30,9 +35,8 @@ for root, dirs, files in os.walk(BASE_DIR):
             normalized_audio = audio.apply_gain(gain)
 
             # Save the normalized audio to a new file
-            normalized_file_path = os.path.join(root, file)
+            normalized_file_path = os.path.join(out_subdir, file)
             normalized_audio.export(normalized_file_path, format="mp3")
-            shutil.move(normalized_file_path, TARGET_DIR)
 
 
 # This script uses the os and pydub modules to normalize audio files within a directory and its subdirectories.
