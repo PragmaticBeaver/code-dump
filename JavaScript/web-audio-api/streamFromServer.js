5591049c42beb7c7a230d290c8679085fd28e7c6
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2023-08-09 00:24:11 +0200

    WIP: stream audio via socket.io

---
+++ JavaScript/web-audio-api/streamFromServer.js
@@ -0,0 +1,89 @@
+console.log("hello world");
+
+let mediaRec = undefined; // for streaming
+
+const audioReceiverElement = document.getElementById("audioReceiver");
+let receiverAudioContext = undefined; // for receiving audio blobs
+
+const socket = io("http://localhost:4000");
+socket.on("audio", async (audioData) => {
+  console.log("received - audio", audioData);
+  const blob = new Blob([audioData]);
+  const buffer = await blob.arrayBuffer();
+
+  receiverAudioContext.decodeAudioData(buffer, (decodedData) => {
+    const source = receiverAudioContext.createBufferSource();
+    source.buffer = decodedData;
+    source.connect(receiverAudioContext.destination);
+    source.start();
+  });
+});
+socket.on("test-back", (data) => {
+  console.log("test-back", data);
+});
+
+const initBtn = document.getElementById("initBtn");
+initBtn.addEventListener("click", (event) => {
+  console.log("initBtn", event);
+
+  /**
+   * ==> init receiver code <==
+   */
+
+  receiverAudioContext = new AudioContext();
+
+  /**
+   * ==> init sender code <==
+   */
+
+  const audioContext = new AudioContext();
+  // Check if context is in suspended state (autoplay policy)
+  if (audioContext.state === "suspended") {
+    audioContext.resume();
+  }
+  const audioSource = document.getElementById("audioSource");
+  const audioSourceNode = audioContext.createMediaElementSource(audioSource);
+  audioSourceNode.connect(audioContext.destination);
+
+  // stream data to server
+  const streamDestination = audioContext.createMediaStreamDestination();
+  audioSourceNode.connect(streamDestination);
+  mediaRec = new MediaRecorder(streamDestination.stream);
+
+  mediaRec.ondataavailable = async (event) => {
+    console.log("ondataavailable", event.data);
+    socket.emit("audio", event.data);
+  };
+
+  mediaRec.onstop = (event) => {
+    console.log("onstop", event);
+  };
+});
+
+const startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", (event) => {
+  console.log("startBtn", event);
+  if (!mediaRec) return;
+
+  if (mediaRec.state === "paused") {
+    mediaRec.resume();
+  } else {
+    mediaRec.start(250);
+  }
+});
+
+const pauseBtn = document.getElementById("pauseBtn");
+pauseBtn.addEventListener("click", (event) => {
+  console.log("pauseBtn", event);
+  if (mediaRec) {
+    mediaRec.pause();
+  }
+});
+
+const stopBtn = document.getElementById("stopBtn");
+stopBtn.addEventListener("click", (event) => {
+  console.log("stopBtn", event);
+  if (mediaRec) {
+    mediaRec.stop();
+  }
+});
